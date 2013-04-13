5b250b5e3ae81ae905a7eb4014706d6d71cb9ee3
minhtrangvy <mvy@hmc.edu>
2013-04-12 22:25:21 -0700

    Update models.py

--- hackathon2013/fiveCAdvantage/models.py
+++ hackathon2013/fiveCAdvantage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+  username =
+    models.CharField(max_length=200)
+  icecream =
+    models.CharField(max_length=200)
